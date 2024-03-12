6ca7c08402bb0f4c9d4051ca5fae7003c2d96e5f
jadenPete <jadenpete@github.com>
2024-03-11 23:54:39 -0400

    Completed Program 3

--- program3.hs
+++ program3.hs
@@ -1,2 +1,36 @@
+import Control.Parallel
+import Data.Maybe
+import Data.String
+import System.IO
+import Text.Printf
+import Text.Read
+
+getMaximumSummand :: IO Int
+getMaximumSummand = do
+    nString <- prompt "Enter an integer n: "
+    maybe getMaximumSummand return (readMaybe nString :: Maybe Int)
+
+prompt :: String -> IO String
+prompt message = do
+    putStr message
+    hFlush stdout
+    getLine
+
+triangularNumber :: Int -> Int
+triangularNumber = triangularNumberFrom 1
+
+triangularNumberFrom :: Int -> Int -> Int
+triangularNumberFrom i j
+    | i > j = 0
+    | i == j = i
+    | otherwise =
+        triangularNumberFrom i midpoint `par`
+        triangularNumberFrom (midpoint + 1) j `pseq`
+        triangularNumberFrom i midpoint + triangularNumberFrom (midpoint + 1) j
+        where midpoint = i + (j - i) `div` 2
+
 main :: IO ()
-main = putStrLn "Hello, world!"
+main = do
+    n <- getMaximumSummand
+    printf "Computing 1 + ... + %d in parallel...\n" n
+    print (triangularNumber n)
